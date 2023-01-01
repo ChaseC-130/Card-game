5cca559ff5de6e1fe858b6b8cdae26b9d70b0506
Kingjerim <k9collector5@gmail.com>
2022-12-31 23:40:55 -0600

    did neato things

---
+++ card-backend/Gameplay/playerstates.js
@@ -0,0 +1,28 @@
+function changeMana (player , amount){
+player.mana = player.mana + amount
+}
+function changeHand (player , card , add){
+    if (add) {
+        player.hand = player.hand + card
+    } else {
+        player.hand = player.hand - card
+    }
+   
+}
+function changeDeck (player , card , add){
+    if (add) {
+        player.deck = player.deck + card
+    } else {
+        player.deck = player.deck - card
+    } 
+}
+function getCardsFromDeck (player , amount){
+    var arr = []
+    for (let i = 0; i<player.deck.length || i < amount; i++) {
+arr.push(player.deck[i])
+    }
+    return arr
+}
+function getDeckSize (player){
+    return player.deck.length
+}
